36ed38cb3d753b138f0182174f2906f28294fdc2
AngelSantiagoPK <angelsantiago094@gmail.com>
2025-01-29 16:33:08 -0500

    changes

---
+++ Scripts/Resources/enemy_weapon_stats.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name WeaponStats
+
+#Data
+@export var projectile_damage : int 
+@export var rate_of_fire : int
+@export var clip_size : int
+@export var bullet_type : PackedScene
+@export var sprite : Texture2D
+
+@export var debug_name : String
